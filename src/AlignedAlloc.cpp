00b7c48464d3f38f2a9bc5a5dcf38de30fdd2e6e
timonent <tatu.timonen@gmail.com>
2021-10-28 23:03:33 +0300

    Use fused multiply-add

--- src/AlignedAlloc.cpp
+++ src/AlignedAlloc.cpp
@@ -1,27 +1,26 @@
 #include "AlignedAlloc.hpp"
 
-namespace AlignedAlloc {
-    
+namespace AlignedAlloc
+{
     __m256d* alloc(std::size_t size)
     {
-    #ifdef _WIN32
+#ifdef _WIN32
         return (__m256d*)_aligned_malloc(sizeof(__m256d)*size, sizeof(__m256d));
-    #else
+#else
         void* ptr = nullptr;
         if (posix_memalign(&ptr, sizeof(__m256d), sizeof(__m256d)*size)) {
             throw std::bad_alloc();
         }
         return (__m256d*)ptr;
-    #endif
+#endif
     }
 
     void free(__m256d* ptr)
     {
-    #ifdef _WIN32
+#ifdef _WIN32
         _aligned_free(ptr);
-    #else
+#else
         std::free(ptr);
-    #endif
+#endif
     }
-
 } // namespace AlignedAlloc
--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,30 @@
 
 int main(int argc, char* const* argv)
 {
-    if (argc != 3) {
+    if (argc != 3)
+    {
         std::cout << "Usage: " << argv[0] << " <input> <output>" << std::endl;
         return 1;
     }
-    const char* input_path = argv[1];
-    const char* output_path = argv[2];
-    cv::Mat img_in = cv::imread(input_path);
+    const char* inputPath = argv[1];
+    const char* outputPath = argv[2];
+    const cv::Mat imgIn = cv::imread(inputPath);
 
     std::cout << "Segmenting...\n";
-    auto t0 = std::chrono::high_resolution_clock::now();
-    auto r = Segment::segment(img_in);
-    auto t1 = std::chrono::high_resolution_clock::now();
-    auto time_taken = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t0);
-    std::cout << "Took " << std::setprecision(2) << time_taken.count() << " seconds, writing results...\n";
+
+    using namespace std::chrono_literals;
+    const auto t0 = std::chrono::high_resolution_clock::now();
+    const auto r = Segment::segment(imgIn);
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    const auto timeTaken = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t0);
     
-    cv::Mat img_out = cv::Mat(img_in.size(), CV_32FC3, cv::Scalar(r.outer[2], r.outer[1], r.outer[0]));
-    cv::rectangle(img_out, cv::Point(r.x0, r.y0), cv::Point(r.x1, r.y1),
+    std::cout << "Took " << std::setprecision(2) << timeTaken.count() << " seconds, writing results...\n";
+    
+    const cv::Mat imgOut = cv::Mat(imgIn.size(), CV_32FC3, cv::Scalar(r.outer[2], r.outer[1], r.outer[0]));
+    cv::rectangle(imgOut, cv::Point(r.x0, r.y0), cv::Point(r.x1, r.y1),
                   cv::Scalar(r.inner[2], r.inner[1], r.inner[0]), cv::FILLED);
-    cv::imwrite(output_path, img_out*255.0);
+    cv::imwrite(outputPath, imgOut*255.0);
+
     std::cout << "Done." << std::endl;
 
     return 0;
